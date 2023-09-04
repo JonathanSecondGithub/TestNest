34ae7f905dd892ba3fa334433af3d92b55f27b92
Jonathan <kariuki.jonathan@students.jkuat.ac.ke>
2023-09-04 23:35:30 +0300

    baseset

--- base.py
+++ base.py
@@ -1,8 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-        return 'Hello, World!'
+def index():
+        return render_template('index.html')
+
+#routes for teachers
+@app.route('/teacher')
+def teacher():
+            return render_template('teacher.html')
+
+@app.route('/teacher/createtest')
+def creattest():
+                return render_template('creattest.html')
+
+@app.route('/teacher/browse')
+def teacherbrowse():
+                return render_template('teacherbrowse.html')
+
+#routes for students
+@app.route('/student')
+def student():
+            return render_template('student.html')
+
+@app.route('/student/test')
+def studenttest():
+                return render_template('studenttest.html')
+
+
