fcc8c13dd10d4da173b727540a4bddba2a46856b
jonathan <kariuki.jonathan@students.jkuat.ac.ke>
2023-09-20 18:06:25 +0300

    bases

--- base.py
+++ base.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, Response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+#database importations
+'''
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity#, jwt_optional
+from config import blacklist
+from models.Post import Post
+from config.lang_config import langs
+from flask_mongoengine import MongoEngine
+from models.User import User
+from routes.api import api
+from routes.user import user
+from routes.post import post
+import random
+'''
+'''
+MAX_POSTS_GLOBAL_WALL = 100
+MAX_POSTS_DASHBOARD = 100
 
+db = MongoEngine()
+'''
+
+
+app = Flask(__name__)
+'''
+app.config['JWT_SECRET_KEY'] = '#OSTL@10@19@25@27#'
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config['JWT_COOKIE_SECURE'] = False
+app.config['JWT_COOKIE_CSRF_PROTECT'] = True
+
+
+jwt = JWTManager(app)
+db.init_app(app)
+
+app.register_blueprint(api)
+app.register_blueprint(user)
+app.register_blueprint(post)
+'''
 @app.route('/')
 def index():
     return render_template('homepage.html', logged_in=True)
@@ -23,10 +54,30 @@ def signup():
 
 @app.route('/compiler')
 def compiler():
-    return render_template('compiler.html', langs=langs, logged_in=True)
+        return render_template('compiler.html')
 
-
-
+@app.route('/dashboard')
+#@jwt_required
+def dashboard():
+    '''
+    user = User.objects(username=get_jwt_identity())[0].to_mongo()
+    posts = []
+    for id in user['following']:
+        u = User.objects(id=id)[0].to_mongo()
+        for postid in u['posts']:
+            if postid not in user['posts']:
+                p = Post.objects(ID=postid)[0].to_mongo()
+                p['by'] = u['username']
+                posts.append(p)
+    posts = posts[:MAX_POSTS_DASHBOARD]
+    random.shuffle(posts)
+    items = []
+    x = Post.objects(originalPostBy=get_jwt_identity())
+    for post in x:
+        item = post.to_mongo()
+        items.append(item)
+    '''
+    return render_template('dashboard.html')
 
 #routes for teachers
 @app.route('/teacher')
